a0ec0255f308eb39e84c9c2146c316ad43485562
3543414437 <83794314+3543414437@users.noreply.github.com>
2023-07-30 20:28:31 +0800

    Add files via upload

---
+++ js/练习.js
@@ -0,0 +1,11 @@
+console.log('hello,world！')
+let name = 'JunBao'
+// console.log(name)
+let firstName ='WEI',lastName ='JunBao'; 
+console.log(firstName,lastName)
+
+let person ={
+        name:'JunBao',
+        age:18
+};
+console.log(person)
